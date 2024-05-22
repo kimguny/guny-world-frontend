cae454fae4afce94b16818037fa2d5d7bc6a62e4
kimguny <kimguny12@gmail.com>
2024-05-22 17:14:52 +0900

    🚑️ 문구 수정

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,11 +3,13 @@ import React from "react";
 
 export default function NotFound() {
   return (
-    <>
-      <h2>아직 개발중이거나 찾을 수 없는 페이지입니다.</h2>
-      <h3>
-        <Link href={"/save/main"}>돌아가기 →</Link>
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <h2 className="text-2xl mb-4">페이지가 존재하지 않습니다.</h2>
+      <h3 className="text-xl">
+        <Link href={"/save/main"} className="text-blue-500 hover:underline">
+          돌아가기 →
+        </Link>
       </h3>
-    </>
+    </div>
   );
 }
