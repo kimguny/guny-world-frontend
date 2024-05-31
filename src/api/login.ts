c2646f7ac2219c66d9791f5c2f37996841981d30
kimguny <kimguny12@gmail.com>
2024-05-31 11:43:43 +0900

    🚧 url 경로 작업중

--- src/api/login.ts
+++ src/api/login.ts
@@ -1,10 +1,8 @@
 import { APIResponse, LoginContent } from "@/types/login";
 import axios from "axios";
 
-const apiBaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
-
 export const login = async (data: { user_id: string; password: string }) => {
-  const res = await axios.post<APIResponse<LoginContent>>(`${apiBaseUrl}/login`, data);
+  const res = await axios.post<APIResponse<LoginContent>>(`/login`, data);
 
   return res.data;
 };
