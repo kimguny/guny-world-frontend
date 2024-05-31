a595b458402112e93448b0bea41823a2922aaa1f
kimguny <kimguny12@gmail.com>
2024-05-31 15:05:42 +0900

    🚑️ /api 수정

--- src/api/login.ts
+++ src/api/login.ts
@@ -1,10 +1,9 @@
-// api/login.ts
 import { APIResponse } from "@/types/login";
 import axios from "axios";
 
 const apiBaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 export const login = async (data: { user_id: string; password: string }) => {
-  const res = await axios.post<APIResponse>(`${apiBaseUrl}/login`, data);
+  const res = await axios.post<APIResponse>(`${apiBaseUrl}/api/login`, data);
   return res.data;
 };
