ca2f8946acc65afe4dbd190ec1a3a7b40e8b88be
kimguny <kimguny12@gmail.com>
2024-06-03 17:31:07 +0900

    🎨 미들웨어 경로 수정하여 적용되게

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,21 +7,22 @@ export function middleware(req: NextRequest) {
 
   console.log(`Pathname: ${pathname}, Token: ${token}`);
 
-  const publicRoutes = ["/login", "/register", "/about"];
+  const publicRoutes = ["/login", "/register"];
 
   if (publicRoutes.includes(pathname)) {
+    console.log("Public route, allowing access");
     return NextResponse.next();
   }
 
   if (!token) {
     const url = req.nextUrl.clone();
     url.pathname = "/login";
+    console.log("Redirecting to /login");
     return NextResponse.redirect(url);
   }
 
+  console.log("Authenticated request, allowing access");
   return NextResponse.next();
 }
 
-export const config = {
-  matcher: ["/:path*"],
-};
+export const config = { matcher: ["/", "/save/:path*"] };
