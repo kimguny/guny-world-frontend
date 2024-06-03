0fd57471a05825930e8f581d483d4141d02786c7
kimguny <kimguny12@gmail.com>
2024-06-03 17:31:48 +0900

    🚑️ Next Config 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,14 @@
+import withFonts from "next-fonts";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withFonts({
+  reactStrictMode: true,
   env: {
     NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
   },
-};
+  webpack(config, options) {
+    return config;
+  },
+});
 
 export default nextConfig;
