88339d68c585a33c42774496dcf9505e15993d21
kimguny <kimguny12@gmail.com>
2024-06-10 17:56:26 +0900

    🚑️ url 수정

--- src/api/client/userInfo.ts
+++ src/api/client/userInfo.ts
@@ -2,6 +2,6 @@ import apiClient from "@/api/apiClient";
 
 // 사용자 정보를 가져오는 함수
 export const userInfo = async () => {
-  const response = await apiClient.get("/api/nickname");
+  const response = await apiClient.get("/api/user_info");
   return response.data;
 };
