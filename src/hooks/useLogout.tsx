f52e832c7fe79db5a2d597d7a199471745c2a07c
kimguny <kimguny12@gmail.com>
2024-06-25 10:13:46 +0900

    ✨ 로그아웃 훅 추가

---
+++ src/hooks/useLogout.tsx
@@ -0,0 +1,16 @@
+import { removeCookie } from "@/utils/cookies";
+import { useRouter } from "next/navigation";
+import { useCallback } from "react";
+
+export default function useLogout() {
+  const router = useRouter();
+
+  const logout = useCallback(() => {
+    removeCookie("accessToken", { path: "/" });
+    localStorage.removeItem("refreshToken");
+    alert("로그아웃 되었습니다. 로그인 페이지로 이동합니다.");
+    router.push("/login");
+  }, [router]);
+
+  return logout;
+}
