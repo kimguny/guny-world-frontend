3a391381d26f75552a67a4afe48ced50f3ebb6ba
kimguny <kimguny12@gmail.com>
2024-08-24 11:18:21 +0900

    🎨 사이드바 map 처리

--- src/components/Layout/Sidebar.tsx
+++ src/components/Layout/Sidebar.tsx
@@ -24,78 +24,48 @@ export default function Sidebar() {
     setSidebarOpen(false);
   };
 
-  const isActive = (path: any) => {
+  const isActive = (path: string) => {
     return pathname === path
       ? "bg-gradient-yellow-active dark:bg-gradient-dark-active"
       : "";
   };
+
+  const sidebarItems = [
+    { href: "/save/main", label: "Main" },
+    { href: "/save/chzzk", label: "Chzzk" },
+    { href: "/404", label: "404" },
+  ];
 
   return (
     <div
       id="sidebar"
       className={`fixed md:relative transform ${
         isSidebarOpen ? "translate-x-0" : "-translate-x-full"
-      } md:translate-x-0 p-6 w-60 bg-light-yellow text-black dark:bg-gray-800 dark:text-white flex-shrink-0 h-full z-50 transition-transform duration-500 ease-in-out`}
+      } md:translate-x-0 p-4 w-60 bg-light-yellow text-black dark:bg-gray-800 dark:text-white flex-shrink-0 h-full z-50 transition-transform duration-500 ease-in-out`}
     >
-      <div className="flex flex-col items-center mb-4">
-        <span className="text-lg font-semibold mt-2">
+      <div className="flex flex-col items-center bg-light-yellow h-14 border border-slate-400 rounded-2xl bg-white dark:bg-gray-800 justify-center">
+        <span className="text-lg font-semibold">
           {nickname ? nickname : "Loading..."}
         </span>
       </div>
 
-      <ul className="list-none p-0">
-        <li
-          className={`my-4 rounded-3xl relative overflow-hidden ${isActive(
-            "/save/main",
-          )} hover:bg-gradient-yellow-hover dark:hover:bg-gradient-dark-hover active:bg-gradient-yellow-active dark:active:bg-gradient-dark-active`}
-        >
-          <Link
-            href="/save/main"
-            className="block text-inherit no-underline rounded-3xl px-4 py-2 relative z-10 dark:text-white"
-            onClick={handleCloseSidebar}
+      <ul className="list-none p-0 pt-4">
+        {sidebarItems.map((item) => (
+          <li
+            key={item.href}
+            className={`my-4 rounded-3xl relative overflow-hidden ${isActive(
+              item.href,
+            )} hover:bg-gradient-yellow-hover dark:hover:bg-gradient-dark-hover active:bg-gradient-yellow-active dark:active:bg-gradient-dark-active`}
           >
-            Main
-          </Link>
-        </li>
-        <li
-          className={`my-4 rounded-3xl relative overflow-hidden ${isActive(
-            "/save/chzzk",
-          )} hover:bg-gradient-yellow-hover dark:hover:bg-gradient-dark-hover active:bg-gradient-yellow-active dark:active:bg-gradient-dark-active`}
-        >
-          <Link
-            href="/save/chzzk"
-            className="block text-inherit no-underline rounded-3xl px-4 py-2 relative z-10 dark:text-white"
-            onClick={handleCloseSidebar}
-          >
-            Chzzk
-          </Link>
-        </li>
-        {/* <li
-          className={`my-4 rounded-3xl relative overflow-hidden ${isActive(
-            "/save/my-info",
-          )} hover:bg-gradient-yellow-hover dark:hover:bg-gradient-dark-hover active:bg-gradient-yellow-active dark:active:bg-gradient-dark-active`}
-        >
-          <Link
-            href="/save/my-info"
-            className="block text-inherit no-underline rounded-3xl px-4 py-2 relative z-10 dark:text-white"
-            onClick={handleCloseSidebar}
-          >
-            MyInfo
-          </Link>
-        </li> */}
-        <li
-          className={`my-4 rounded-3xl relative overflow-hidden ${isActive(
-            "/404",
-          )} hover:bg-gradient-yellow-hover dark:hover:bg-gradient-dark-hover active:bg-gradient-yellow-active dark:active:bg-gradient-dark-active`}
-        >
-          <Link
-            href="/404"
-            className="block text-inherit no-underline rounded-3xl px-4 py-2 relative z-10 dark:text-white"
-            onClick={handleCloseSidebar}
-          >
-            404
-          </Link>
-        </li>
+            <Link
+              href={item.href}
+              className="block text-inherit no-underline rounded-3xl px-4 py-2 relative z-10 dark:text-white"
+              onClick={handleCloseSidebar}
+            >
+              {item.label}
+            </Link>
+          </li>
+        ))}
       </ul>
     </div>
   );
