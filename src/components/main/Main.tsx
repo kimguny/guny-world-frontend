1f32fbb3d1386607296c37b8553f3567d758039c
kimguny <kimguny12@gmail.com>
2024-09-02 13:49:40 +0900

    🚑️ 긴급 핫픽스

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -2,8 +2,7 @@ export default function Main() {
   return (
     <>
       <div className="dark:bg-gray-800 dark:text-white">
-        현재 네이버 API가 변경됨에 따라 기능에서 에러가 나오고 있습니다. 추후
-        업데이트 후 공지 남기겠습니다. ㅠ
+        아직 개발중이라 오류가 있을 수 있습니다.
       </div>
     </>
   );
