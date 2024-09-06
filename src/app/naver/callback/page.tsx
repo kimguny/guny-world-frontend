6016b087c88a3e9aa20463500430a65c6d7d446b
kimguny <kimguny12@gmail.com>
2024-09-06 13:47:46 +0900

    ✨ 콜백 페이지 추가

---
+++ src/app/naver/callback/page.tsx
@@ -0,0 +1,5 @@
+import NaverCallback from "@/components/common/NaverCallback";
+
+export default function NaverCallbackPage() {
+  return <NaverCallback />;
+}
--- src/components/common/NaverCallback.tsx
+++ src/components/common/NaverCallback.tsx
@@ -1,25 +1,27 @@
 "use client";
 
-import useNaverLoginMutation from "@/hooks/mutation/useNaverLoginMutation";
 import { useRouter } from "next/navigation";
+import { setCookie } from "@/utils/cookies";
 import { useEffect } from "react";
 
 export default function NaverCallback() {
   const router = useRouter();
-  const { mutate: postNaverLogin } = useNaverLoginMutation();
 
   useEffect(() => {
     const query = new URLSearchParams(window.location.search);
-    const code = query.get("code");
-    const state = query.get("state");
+    const accessToken = query.get("accessToken");
+    const refreshToken = query.get("refreshToken");
 
-    if (code && state) {
-      postNaverLogin({ code, state });
+    if (accessToken && refreshToken) {
+      setCookie("accessToken", accessToken, { path: "/" });
+      localStorage.setItem("refreshToken", refreshToken);
+
+      router.push("/save/main");
     } else {
       alert("잘못된 접근입니다.");
       router.push("/login");
     }
-  }, [postNaverLogin, router]);
+  }, [router]);
 
   return null;
 }
