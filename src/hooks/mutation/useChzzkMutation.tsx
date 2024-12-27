43b147e755b90f5730f7826c6f2e8bbee438e293
kimguny <kimguny12@gmail.com>
2024-12-27 14:28:14 +0900

    🐛 chzzk 에러처리 수정

--- src/hooks/mutation/useChzzkMutation.tsx
+++ src/hooks/mutation/useChzzkMutation.tsx
@@ -1,17 +1,21 @@
 import { useMutation, UseMutationResult } from "@tanstack/react-query";
 import { ChzzkResponse, FetchFollowersParams } from "@/types/chzzk";
 import { useChzzk } from "@/api/client/useChzzk";
+import { AxiosError } from "axios";
 
 export default function useChzzkMutation(
   onSuccessCallback?: (data: ChzzkResponse) => void,
-): UseMutationResult<ChzzkResponse, unknown, FetchFollowersParams, unknown> {
-  return useMutation<ChzzkResponse, unknown, FetchFollowersParams>({
+): UseMutationResult<ChzzkResponse, AxiosError, FetchFollowersParams, unknown> {
+  return useMutation<ChzzkResponse, AxiosError, FetchFollowersParams>({
     mutationFn: useChzzk,
     onSuccess: (data) => {
       onSuccessCallback?.(data);
     },
     onError: (error) => {
-      alert(error);
+      const errorMessage =
+        (error.response?.data as { error?: string })?.error ||
+        "알 수 없는 에러가 발생했습니다.";
+      alert(errorMessage);
     },
   });
 }
